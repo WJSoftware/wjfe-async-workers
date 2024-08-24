3159e4b201877fa92e648af9e2b4c6ee1600d746
Ramirez Vargas, José Pablo <webJose@gmail.com>
2024-08-24 11:50:05 -0600

    feat(demo):  Add devhunt.org banner

---
+++ pages/src/lib/TopBanner.svelte
@@ -0,0 +1,19 @@
+<div class="banner px-5 py-3 text-bg-primary fs-5">
+    <p>
+        Cast your vote @ <a href="https://devhunt.org/tool/wjfeasync-workers" target="_blank">devhunt.org</a> on 
+        <strong>March the 4<sup>th</sup>, 2025.</strong>
+    </p>
+</div>
+
+<style>
+    .banner {
+        width: 100%;
+        text-align: center;
+    }
+    .banner a {
+        color: var(--bs-white);
+    }
+    .banner p {
+        margin-bottom: 0;
+    }
+</style>
