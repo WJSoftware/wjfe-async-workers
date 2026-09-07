47df8472145095c15281f1b79d66b696da97f69d
Ramirez Vargas, José Pablo <webJose@gmail.com>
2026-09-07 11:03:47 -0600

    feat: Add instance methods

--- src/sync/Mutex.ts
+++ src/sync/Mutex.ts
@@ -7,11 +7,10 @@ import { acquireSync, acquire, SemaphoreInternal, type Releaser } from "./Semaph
  */
 export class Mutex extends SemaphoreInternal {
     constructor(createDisabled: boolean = false) {
-        super(mutexIdentityData[0], 1, createDisabled);
+        super(mutexIdentityData, 1, createDisabled);
     }
-
     /**
-     * Acquires exclusivity from the specified mutex's token.
+     * Acquires exclusivity over the specified mutex's token.
      * 
      * **IMPORTANT**:  Acquiring a mutex halts other work.  Releasing the mutex by invoking the releaser function that 
      * this method returns is *imperative*.  Use a `try..finally` construct to make sure that the releaser function is 
@@ -19,7 +18,7 @@ export class Mutex extends SemaphoreInternal {
      * 
      * @example
      * ```typescript
-     * const releaser = await Mutex.acquire(myMutexToken);
+     * const releaser = Mutex.acquireSync(myMutexToken);
      * try {
      *     ...
      * }
@@ -28,11 +27,11 @@ export class Mutex extends SemaphoreInternal {
      * }
      * ```
      * @param token Mutex token to be acquired.
-     * @returns A releaser object that can and should be used for releasing the mutex.
+     * @returns A releaser function that can and should be used for releasing the mutex.
      */
     static acquireSync(token: Token): Releaser;
     /**
-     * Acquires exclusivity from the specified mutex's token.
+     * Acquires exclusivity over the specified mutex's token.
      * 
      * **IMPORTANT**:  Acquiring a mutex halts other work.  Releasing the mutex by invoking the releaser function that 
      * this method returns is *imperative*.  Use a `try..finally` construct to make sure that the releaser function is 
@@ -40,23 +39,71 @@ export class Mutex extends SemaphoreInternal {
      * 
      * @example
      * ```typescript
-     * const releaser = await Mutex.acquire(myMutexToken);
-     * try {
+     * const releaser = Mutex.acquireSync(myMutexToken);
+     * if (releaser !== "timed-out") {
+     *   try {
      *     ...
-     * }
-     * finally {
+     *   }
+     *   finally {
      *     releaser();
+     *   }
      * }
      * ```
      * @param token Mutex token to be acquired.
      * @param timeout Timeout value in milliseconds to wait for acquisition.
-     * @returns A releaser object that can and should be used for releasing the mutex, or the value `'timed-out'` if 
+     * @returns A releaser function that can and should be used for releasing the mutex, or the value `'timed-out'` if 
      * the mutex could not be acquired before the specified timeout time elapsed.
      */
     static acquireSync(token: Token, timeout: number): Releaser | "timed-out";
     static acquireSync(token: Token, timeout?: number) {
         return acquireSync(mutexIdentityData, token, timeout);
     }
+    /**
+     * Asynchronously acquires exclusivity over the specified mutex's token.
+     * 
+     * **IMPORTANT**:  Acquiring a mutex halts other work.  Releasing the mutex by invoking the releaser function that 
+     * this method returns is *imperative*.  Use a `try..finally` construct to make sure that the releaser function is 
+     * always executed, even in the event of an error.
+     * 
+     * @example
+     * ```typescript
+     * const releaser = await Mutex.acquire(myMutexToken);
+     * try {
+     *     ...
+     * }
+     * finally {
+     *     releaser();
+     * }
+     * ```
+     * @param token Mutex token to be acquired.
+     * @returns A releaser function that can and should be used for releasing the mutex.
+     */
+    static acquire(token: Token): Promise<Releaser>;
+    /**
+     * Asynchronously acquires exclusivity over the specified mutex's token.
+     * 
+     * **IMPORTANT**:  Acquiring a mutex halts other work.  Releasing the mutex by invoking the releaser function that 
+     * this method returns is *imperative*.  Use a `try..finally` construct to make sure that the releaser function is 
+     * always executed, even in the event of an error.
+     * 
+     * @example
+     * ```typescript
+     * const releaser = await myMutex.acquire(500);
+     * if (releaser !== "timed-out") {
+     *   try {
+     *     ...
+     *   }
+     *   finally {
+     *     releaser();
+     *   }
+     * }
+     * ```
+     * @param token Mutex token to be acquired.
+     * @param timeout Timeout value in milliseconds to wait for acquisition.
+     * @returns A releaser function that can and should be used for releasing the mutex, or the value `'timed-out'` if 
+     * the mutex could not be acquired before the specified timeout time elapsed.
+     */
+    static acquire(token: Token, timeout: number): Promise<Releaser | "timed-out">;
     static acquire(token: Token, timeout?: number) {
         return acquire(mutexIdentityData, token, timeout);
     }
